8bf42ca70bbf12c1c1b21f5624980a2143c65e78
Prabu Sathan <prabu.sathan@gmail.com>
2019-09-30 20:39:12 +0100

    Second endpoint call

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//cors url
 const cors = "https://cors-anywhere.herokuapp.com/";
 
+//first endpoint url request
 fetch(
   `${cors}https://u0e8utqkk2.execute-api.eu-west-2.amazonaws.com/dev/email-service/health`
 )
@@ -7,5 +9,16 @@ fetch(
   .then(res => {
     const first = document.getElementById("first");
     first.innerHTML = res.status;
+  })
+  .catch(err => console.log("error occured", err));
+
+//second endpoint url request
+fetch(
+  `${cors}https://u0e8utqkk2.execute-api.eu-west-2.amazonaws.com/dev/payment-gateway/health`
+)
+  .then(res => res.text())
+  .then(res => {
+    const second = document.getElementById("second");
+    second.innerHTML = res;
   })
   .catch(err => console.log("error occured", err));
