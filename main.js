05bc1aa027ff5db99a8ea800c108adc340525da2
Prabu Sathan <prabu.sathan@gmail.com>
2019-09-30 21:05:28 +0100

    Third Endpoint call

--- main.js
+++ main.js
@@ -14,11 +14,26 @@ fetch(
 
 //second endpoint url request
 fetch(
-  `${cors}https://u0e8utqkk2.execute-api.eu-west-2.amazonaws.com/dev/payment-gateway/health`
+  `https://u0e8utqkk2.execute-api.eu-west-2.amazonaws.com/dev/payment-gateway/health`
 )
   .then(res => res.text())
   .then(res => {
     const second = document.getElementById("second");
     second.innerHTML = res;
+  })
+  .catch(err => console.log("error occured", err));
+
+//third endpoint url request
+fetch(
+  `https://u0e8utqkk2.execute-api.eu-west-2.amazonaws.com/dev/microservice-controller/health`,
+  {
+    method: "POST"
+  }
+)
+  .then(res => res.text())
+  .then(res => {
+    const third = document.getElementById("third");
+    third.innerHTML = res;
+    console.log(res);
   })
   .catch(err => console.log("error occured", err));
